f0005125618a9bba69fb024425d80f7346f04a1d
liobouchan <leo.bouchan@gmail.com>
2015-07-01 01:24:09 -0500

    Array Solutions

---
+++ Javascripting/arrays.js
@@ -0,0 +1,27 @@
+/*# ARRAYS
+
+An array is a list of values. Here's an example:
+
+var pets = ['cat', 'dog', 'rat'];
+
+### The challenge:
+
+Create a file named arrays.js.
+
+In that file define a variable named pizzaToppings that references an array that contains three strings in this order: tomato sauce, cheese, pepperoni.
+
+Use console.log() to print the pizzaToppings array to the terminal.
+
+Check to see if your program is correct by running this command:
+
+javascripting verify arrays.js
+
+-------------------
+
+ Â» To print these instructions again, run: javascripting print
+ Â» To execute your program in a test environment, run: javascripting run program.js
+ Â» To verify your program, run: javascripting verify program.js
+ Â» For help run: javascripting help
+*/
+var pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni'];
+console.log(pizzaToppings);
