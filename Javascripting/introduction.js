58f282587ff326c68d62fe438c0545d49e0c6085
liobouchan <leo.bouchan@gmail.com>
2015-07-01 00:13:02 -0500

    Introduction of Javascripting Ready

---
+++ Javascripting/introduction.js
@@ -0,0 +1,35 @@
+/*# INTRODUCTION
+
+  To keep things organized, let's create a folder for this workshop. 
+
+  Run this command to make a directory called javascripting (or something else if you like):
+
+    mkdir javascripting
+
+  Change directory into the javascripting folder:
+
+    cd javascripting
+
+  Create a file named introduction.js:
+
+    touch introduction.js
+
+  Open the file in your favorite editor, and add this text:
+
+    console.log('hello');
+
+  Save the file, then check to see if your program is correct by running this command:
+
+    javascripting verify introduction.js
+
+  -------------------
+  Need help? View the README for this workshop: [http://github.com/sethvincent/javascripting](http://github.com/sethvincent/javascripting)
+
+
+   Â» To print these instructions again, run: javascripting print
+   Â» To execute your program in a test environment, run: javascripting run program.js
+   Â» To verify your program, run: javascripting verify program.js
+   Â» For help run: javascripting help
+*/
+
+console.log('hello');
