937a0d737c2ec4f204d572f73c7ba0e564241491
Leonardo <liogothicvampire@gmail.com>
2018-10-12 11:22:08 -0500

    Ready Simple

--- manageFiles/index.js
+++ manageFiles/index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 const text = "hello this is a test";
 
-fs.writeFile('./files/file.json', text, (err) => {
+fs.writeFile('./files/file.txt', text, (err) => {
     if (err) throw err;
     console.log("saved Text");
 });
